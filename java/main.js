f2c313f1a55204f71d10aea323dca9312eb9fa19
Abdelrahmanm22 <abdra1396@gmail.com>
2022-02-15 04:07:40 +0200

    first

---
+++ java/main.js
@@ -0,0 +1,22 @@
+$(".dropdown").mouseover(function(){
+    $(this).children(".dropdown-menu").css("display","block");
+})
+$(".dropdown").mouseout(function(){
+    $(this).children(".dropdown-menu").css("display","none");
+})
+
+$(".dropdown-item").mouseover(function(){
+    $(this).css({"background-color":"black","color":"#e4b34c"});
+})
+$(".dropdown-item").mouseout(function(){
+    $(this).css({"background-color":"initial","color":"white"});
+})
+
+
+
+
+const swiper = new Swiper('.swiper', {
+    autoplay: {
+      delay: 7000,
+    },
+   });
